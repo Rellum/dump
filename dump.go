e4cc553995b08e081f7d888b86900a2baadba377
vaughan <vaughan@luno.com>
2021-03-24 12:48:10 +0200

    dump: Add Fdump method

--- dump.go
+++ dump.go
@@ -1,12 +1,18 @@
 package dump
 
 import (
+	"io"
+	"os"
 	"runtime"
 
 	"github.com/davecgh/go-spew/spew"
 )
 
 func Dump(a ...interface{}) {
+	Fdump(os.Stdout, a...)
+}
+
+func Fdump(w io.Writer, a ...interface{}) {
 	caller, file, line, ok := runtime.Caller(1)
-	spew.Dump(append([]interface{}{caller, file, line, ok}, a...)...)
+	spew.Fdump(w, append([]interface{}{caller, file, line, ok}, a...)...)
 }
